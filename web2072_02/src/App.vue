c1c7f9d5d1d0076712d2230e3919736e6a2cd39c
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-05-16 19:56:35 +0700

    ref, reactive

--- web2072_02/src/App.vue
+++ web2072_02/src/App.vue
@@ -1,10 +1,33 @@
 <script setup>
+  import { reactive, ref } from "vue";
+
+  // ref: number, string, boolean
+  const name = ref("chinhpd5");
+  const count = ref(0)
+  
+  const increaseCount = () =>{
+    count.value += 1
+  }
+
+  // reactive : object, array
+  const myInfo = reactive({
+    name: "Phí Đức Chính",
+    age: 20,
+    gender: false
+  })
 
 </script>
 
 <template>
   <div>
-    <h1>Chinhpd5</h1>
+    <!-- ref -->
+    <h1>Họ và tên: {{ name }}</h1>
+    <h1>Count: {{ count }}</h1>
+    <button @click="increaseCount" class="btn btn-primary">Tăng</button>
+    <!-- reactive -->
+    <h1>Họ và tên: {{ myInfo.name }}</h1>
+    <h2>Tuổi: {{ myInfo.age }}</h2>
+    <h2>Giới tính: {{ myInfo.gender ? 'Nam': 'Nữ' }}</h2>
   </div>
 </template>
 
