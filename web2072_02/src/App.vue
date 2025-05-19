e55ca19a90a16444c9a78eadfc08cc9f6cf78433
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-05-19 19:20:54 +0700

    to do list

--- web2072_02/src/App.vue
+++ web2072_02/src/App.vue
@@ -1,11 +1,20 @@
 <script setup>
-  import {ref} from 'vue'
+  import {reactive, ref} from 'vue'
   // one way binding
   const myName = "chinhpd5";
   const imageURL = "https://camerabox.vn/uploads/thuat-ngu-nhiep-anh.jpg"
   const btnClass = "btn btn-primary";
 
-  const myText = ref('')
+  const myText = ref('');
+
+  const jobs = reactive(["Ăn","Ngủ","Code"])
+
+  const handleAdd = () => {
+    // console.log(myText.value);
+    jobs.push(myText.value);
+    myText.value = '';
+      
+  }
   
 </script>
 
@@ -14,14 +23,23 @@
     <!-- one way -->
     <h1>Ràng buộc 1 chiều: {{ myName }}</h1>
     <!-- <img v-bind:src="imageURL" v-bind:alt="myName"> -->
-    <img :src="imageURL" :alt="myName">
+    <!-- <img :src="imageURL" :alt="myName"> -->
     <button :class="btnClass">Click</button>
     <hr>
 
     <!-- two way -->
     <h1>Ràng buộc 2 chiều:</h1>
     <input type="text" v-model="myText">
-    <h1>text: {{ myText }}</h1>
+    <button class="btn btn-primary" v-on:click="handleAdd">Add</button>
+    <!-- <h1>text: {{ myText }}</h1> -->
+
+    <!-- to do list -->
+    <ul>
+      <li v-for="(item,index) in jobs" :key="index">
+        {{ item }}
+        <button class="btn btn-danger">Xóa</button>
+      </li>
+    </ul>
   </div>
 </template>
 
