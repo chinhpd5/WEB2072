34798e0c3b8e8a896d9801c837623e4139d9e353
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-05-21 18:54:54 +0700

    v-if v-else v-else-if

--- web2072_02/src/App.vue
+++ web2072_02/src/App.vue
@@ -1,50 +1,19 @@
 <script setup>
-  import {reactive, ref} from 'vue'
-  // one way binding
-  const myName = "chinhpd5";
-  const imageURL = "https://camerabox.vn/uploads/thuat-ngu-nhiep-anh.jpg"
-  const btnClass = "btn btn-primary";
-
-  const myText = ref('');
-
-  const jobs = reactive(["Ăn","Ngủ","Code"])
-
-  const handleAdd = () => {
-    // console.log(myText.value);
-    jobs.push(myText.value);
-    myText.value = '';
-      
-  }
-
-  const handleDelete = (index) =>{
-    // console.log(index);
-    jobs.splice(index,1);
-  }
-  
+  import {ref} from 'vue'
+  const score = ref(Math.floor(Math.random()*10)*10)
 </script>
 
 <template>
-  <div class="pb-5">
-    <!-- one way -->
-    <h1>Ràng buộc 1 chiều: {{ myName }}</h1>
-    <!-- <img v-bind:src="imageURL" v-bind:alt="myName"> -->
-    <!-- <img :src="imageURL" :alt="myName"> -->
-    <button :class="btnClass">Click</button>
-    <hr>
+  <div>
+    <h1>Điểm: {{ score }}</h1>
+    <h4>
+      Học lực: 
+      <span v-if="score >= 90">Giỏi</span>
+      <span v-else-if="score >= 70">Khá</span>
+      <span v-else-if="score >= 50">Trung bình</span>
+      <span v-else>Yếu</span>
+    </h4>
 
-    <!-- two way -->
-    <h1>Ràng buộc 2 chiều:</h1>
-    <input type="text" v-model="myText">
-    <button class="btn btn-primary" v-on:click="handleAdd">Add</button>
-    <!-- <h1>text: {{ myText }}</h1> -->
-
-    <!-- to do list -->
-    <ul>
-      <li v-for="(item,index) in jobs" :key="index">
-        {{ item }}
-        <button class="btn btn-danger" @click="handleDelete(index)">Xóa</button>
-      </li>
-    </ul>
   </div>
 </template>
 
