ecf24f816725ed71c49416cf23de26130cce9c8f
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-05-21 19:36:48 +0700

    v-for

--- web2072_02/src/App.vue
+++ web2072_02/src/App.vue
@@ -2,7 +2,14 @@
   import {ref} from 'vue'
   const score = ref(Math.floor(Math.random()*10)*10);
 
-  const isLoggin = ref(false)
+  const isLoggin = ref(true);
+
+  const list = [
+    {name: "chinhpd5", age: 20},
+    {name: "chinhpd6", age: 21},
+    {name: "chinhpd7", age: 22},
+  ];
+
 </script>
 
 <template>
@@ -22,11 +29,28 @@
       <div v-show="isLoggin">
         <div class="d-flex">
           <h5>Xin chào, chinhpd5!</h5>
-          <button @click="isLoggin = !isLoggin" class="btn btn-outline-warning">Đăng xuất</button>
+          <button @click="isLoggin = !isLoggin" class="btn btn-outline-warning">
+            Đăng xuất
+          </button>
         </div>
       </div>
 
-      <button @click="isLoggin = !isLoggin" v-show="!isLoggin" class="btn btn-primary">Đăng nhập</button>
+      <button @click="isLoggin = !isLoggin" v-show="!isLoggin" class="btn btn-primary">
+        Đăng nhập
+      </button>
+    </div>
+
+    <hr>
+    <div>
+      <ul>
+        <li v-for="(item,index) in list" :key="index">
+          {{ index+1 }}| <span>Tên: {{ item.name }}</span>| <span>Tuổi: {{ item.age }}</span>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h1>Danh sách sản phẩm:</h1>
     </div>
   </div>
 </template>
