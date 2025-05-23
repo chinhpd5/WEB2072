5bf85ff930df60da27c34df9023cd59f94abe3c4
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-05-23 19:23:58 +0700

    form 1

--- web2072_02/src/App.vue
+++ web2072_02/src/App.vue
@@ -33,6 +33,13 @@
     message.value = text.value
   }
 
+  const product = ref({})
+  const handleSubmit = () =>{
+    // console.log("submit!!!");
+    console.log(product.value);
+    
+  }
+
 
 </script>
 
@@ -54,6 +61,52 @@
       <!-- Sự kiện với bàn phím -->
       <input type="text" @keydown="handleKey" @keydown.enter="showMessage" v-model="text">
       <h1>Text: {{ message }}</h1>
+    </div>
+
+    <div class="container">
+      <!-- Sự kiện với form -->
+       <form @submit.prevent="handleSubmit">
+
+        <div class="mb-3">
+          <label for="id" class="form-label">ID</label>
+          <input type="text" class="form-control" id="id" v-model="product.id" >
+        </div>
+
+        <div class="mb-3">
+          <label for="name" class="form-label">Tên sản phẩm</label>
+          <input type="text" class="form-control" id="name" v-model="product.name" >
+        </div>
+
+        <div class="mb-3">
+          <label for="price" class="form-label">Giá</label>
+          <input type="number" class="form-control" id="price" v-model="product.price" >
+        </div>
+
+        <div class="mb-3">
+          <label for="quantity" class="form-label">Số lượng</label>
+          <input type="number" class="form-control" id="quantity" v-model="product.quantity" >
+        </div>
+
+        <div class="mb-3">
+          <label for="" class="form-label">Trạng thái</label>
+
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="isVisible" v-model="product.isVisible" :value="true" id="active">
+            <label class="form-check-label" for="active">
+              Kích hoạt
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="isVisible" v-model="product.isVisible" :value="false" id="notActive">
+            <label class="form-check-label" for="notActive">
+              Không kích hoạt
+            </label>
+          </div>
+
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
 
 
