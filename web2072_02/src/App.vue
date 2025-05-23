250070e9f4182f9ad782fc0f39947fcfb2c7d054
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-05-23 19:05:41 +0700

    sự kiện với bàn phím

--- web2072_02/src/App.vue
+++ web2072_02/src/App.vue
@@ -16,20 +16,45 @@
   }
 
   const handleReset = () =>{
-    count.value = 0
+    count.value = 0;
   }
+
+  const text = ref('');
+  const message = ref('')
+
+  const handleKey = (event) => {
+    // console.log(event.key);
+    // if(event.key == 'Enter'){
+    //   message.value = text.value
+    // }
+  }
+  
+  const showMessage = () =>{
+    message.value = text.value
+  }
+
+
 </script>
 
 <template>
   <div>
     
-    <!-- <button v-on:click="handleAlert" class="btn btn-primary">show</button> -->
-    <!-- <button @dblclick="handleAlert" class="btn btn-primary">show</button> -->
-    <h1>Count: {{ count }}</h1>
+    <div>
+      <!-- Sự kiện với chuột -->
+      <!-- <button v-on:click="handleAlert" class="btn btn-primary">show</button> -->
+      <!-- <button @dblclick="handleAlert" class="btn btn-primary">show</button> -->
+      <h1>Count: {{ count }}</h1>
 
-    <button @click="handleIncrease" class="btn btn-primary">Tăng</button>
-    <button @click="handleDecrease" class="btn btn-danger">Giảm</button>
-    <button @click="handleReset" class="btn btn-warning">Reset</button>
+      <button @click="handleIncrease" class="btn btn-primary">Tăng</button>
+      <button @click="handleDecrease" class="btn btn-danger">Giảm</button>
+      <button @click="handleReset" class="btn btn-warning">Reset</button>
+    </div>
+
+    <div>
+      <!-- Sự kiện với bàn phím -->
+      <input type="text" @keydown="handleKey" @keydown.enter="showMessage" v-model="text">
+      <h1>Text: {{ message }}</h1>
+    </div>
 
 
     <div>
