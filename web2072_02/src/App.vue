48c55d18c1e71390c5db65d0f9f8312b8bbbb575
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-05-26 19:00:25 +0700

    component

--- web2072_02/src/App.vue
+++ web2072_02/src/App.vue
@@ -1,23 +1,7 @@
 <script setup>
-
   import { ref } from 'vue';
-  const count = ref(0)
- 
-  const handleAlert = () =>{
-    alert("Thông báo")
-  }
-
-  const handleIncrease = () => {
-    count.value++
-  }
-
-  const handleDecrease = () =>{
-    count.value--
-  }
-
-  const handleReset = () =>{
-    count.value = 0;
-  }
+  import HelloComponent from './components/HelloComponent.vue';
+  
 
   const text = ref('');
   const message = ref('')
@@ -50,18 +34,9 @@
 
 <template>
   <div>
+    <HelloComponent />
+    <HelloComponent></HelloComponent>
     
-    <div>
-      <!-- Sự kiện với chuột -->
-      <!-- <button v-on:click="handleAlert" class="btn btn-primary">show</button> -->
-      <!-- <button @dblclick="handleAlert" class="btn btn-primary">show</button> -->
-      <h1>Count: {{ count }}</h1>
-
-      <button @click="handleIncrease" class="btn btn-primary">Tăng</button>
-      <button @click="handleDecrease" class="btn btn-danger">Giảm</button>
-      <button @click="handleReset" class="btn btn-warning">Reset</button>
-    </div>
-
     <div>
       <!-- Sự kiện với bàn phím -->
       <input type="text" @keydown="handleKey" @keydown.enter="showMessage" v-model="text">
---
+++ web2072_02/src/components/HelloComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Xin chào: {{ name }}</h1>
+
+    <div>
+      <!-- Sự kiện với chuột -->
+      <!-- <button v-on:click="handleAlert" class="btn btn-primary">show</button> -->
+      <!-- <button @dblclick="handleAlert" class="btn btn-primary">show</button> -->
+      <h1>Count: {{ count }}</h1>
+
+      <button @click="handleIncrease" class="btn btn-primary">Tăng</button>
+      <button @click="handleDecrease" class="btn btn-danger">Giảm</button>
+      <button @click="handleReset" class="btn btn-warning">Reset</button>
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+  import {ref} from 'vue'
+  const name = "Chinhpd5"
+  const count = ref(0)
+  
+  const handleIncrease = () => {
+    count.value++
+  }
+
+  const handleDecrease = () =>{
+    count.value--
+  }
+
+  const handleReset = () =>{
+    count.value = 0;
+  }
+</script>
+
+<style scoped>
+  .h1{
+    color: red
+  }
+</style>
