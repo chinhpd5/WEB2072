c71308241aa5166d4806d92af6d84f2fd76f44aa
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-07-15 15:58:48 +0700

    ref

--- web2072/src/App.vue
+++ web2072/src/App.vue
@@ -1,7 +1,16 @@
 <script setup>
+import {ref} from 'vue'
+
 const name = "chinhpd5";
 const age= 20;
 const gender = true;
+
+const count = ref(0);
+
+const handleIncrease = () => {
+  count.value++;
+  // console.log(count.value);
+}
 
 </script>
 
@@ -10,6 +19,13 @@ const gender = true;
     <h1 class="text-danger">Họ và tên: {{ name }}</h1>
     <h2>Tuổi: {{ age }}</h2>
     <h3>Giới tính: {{ gender ? "Nam": "Nữ" }}</h3>
+
+    <div>
+      <h1>{{ count }}</h1>
+      <button @click="handleIncrease" class="btn btn-success">Tăng</button>
+      <button class="btn btn-danger">Giảm</button>
+      <button class="btn btn-warning">Reset</button>
+    </div>
   </div>
 </template>
 
