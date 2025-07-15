c5ee56e56f35390a0d58bf874ec2bcc21d614c7e
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-07-15 15:43:57 +0700

    cài đặt dự án

---
+++ web2072/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+const name = "chinhpd5";
+const age= 20;
+const gender = true;
+
+</script>
+
+<template>
+  <div>
+    <h1>Họ và tên: {{ name }}</h1>
+    <h2>Tuổi: {{ age }}</h2>
+    <h3>Giới tính: {{ gender ? "Nam": "Nữ" }}</h3>
+  </div>
+</template>
+
+<style scoped>
+/* scoped khai phạm css chỉ thỏa mãn trong file vue hiện tại */
+</style>
