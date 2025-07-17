de425583943b041df1b56a1b9eb04725779b88fc
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-07-17 15:05:54 +0700

    one-way binding

--- web2072/src/App.vue
+++ web2072/src/App.vue
@@ -1,60 +1,17 @@
-<script setup>
-import {ref,reactive} from 'vue'
-
-// const name = "chinhpd5";
-// const age= 20;
-// const gender = true;
-
-const count = ref(0); // có thể sử dụng với tất cả các kiểu dữ liệu
-let info = reactive({ // chỉ sử dụng với array và object
-  name: "chinhpd5",
-  age: 20,
-  gender: true
-})
-// sử dụng ul li hiển thị mảng myArray
-const myArray = reactive(["Hùng", "Long", "Bình"])
-
-const handleChange = () => {
-  info.name = "xin chào chinhpd5"
-
-  // Không thể gán 1 giá trị mới cho reactive
-  // info = { 
-  //   name: "chinhpd5 new",
-  //   age: 20,
-  //   gender: true
-  // } 
-}
-
-const handleIncrease = () => {
-  count.value++;
-  // console.log(count.value);
-}
-
-const handleDecrease =  () => {
-  count.value--;
-}
-
-const handleReset = () => {
-  count.value = 0;
-}
-</script>
-
 <template>
   <div>
-    <h1 class="text-danger">Họ và tên: {{ info.name }}</h1>
-    <h2>Tuổi: {{ info.age }}</h2>
-    <h3>Giới tính: {{ info.gender ? "Nam": "Nữ" }}</h3>
-    <button class="btn btn-info" @click="handleChange">Change</button>
-    <hr>
-    <div>
-      <h1>{{ count }}</h1>
-      <button @click="handleIncrease" class="btn btn-success">Tăng</button>
-      <button @click="handleDecrease" class="btn btn-danger">Giảm</button>
-      <button @click="handleReset" class="btn btn-warning">Reset</button>
-    </div>
+    <!-- one-way binding -->
+    <h1>Họ và tên: {{ name }}</h1>
+    <img height="100" v-bind:src="imageURL" v-bind:alt="name">
+    <img height="100" :src="imageURL" :alt="name">
   </div>
 </template>
 
-<style scoped>
-/* scoped khai phạm css chỉ thỏa mãn trong file vue hiện tại */
+<script setup>
+const name = "chinhpd5";
+const imageURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGsY-zxNnrhZxqj4a_6Kvuv_tU1oGJWdAY1g&s"
+</script>
+
+<style>
+
 </style>
---
+++ web2072/src/components/ref_reactive.vue
@@ -0,0 +1,66 @@
+<script setup>
+import {ref,reactive} from 'vue'
+
+// const name = "chinhpd5";
+// const age= 20;
+// const gender = true;
+
+const count = ref(0); // có thể sử dụng với tất cả các kiểu dữ liệu
+let info = reactive({ // chỉ sử dụng với array và object
+  name: "chinhpd5",
+  age: 20,
+  gender: true
+})
+// sử dụng ul li hiển thị mảng myArray
+const myArray = reactive(["Hùng", "Long", "Bình"])
+
+const handleChange = () => {
+  info.name = "xin chào chinhpd5"
+
+  // Không thể gán 1 giá trị mới cho reactive
+  // info = { 
+  //   name: "chinhpd5 new",
+  //   age: 20,
+  //   gender: true
+  // } 
+}
+
+const handleIncrease = () => {
+  count.value++;
+  // console.log(count.value);
+}
+
+const handleDecrease =  () => {
+  count.value--;
+}
+
+const handleReset = () => {
+  count.value = 0;
+}
+</script>
+
+<template>
+  <div>
+    <h1 class="text-danger">Họ và tên: {{ info.name }}</h1>
+    <h2>Tuổi: {{ info.age }}</h2>
+    <h3>Giới tính: {{ info.gender ? "Nam": "Nữ" }}</h3>
+    <button class="btn btn-info" @click="handleChange">Change</button>
+    <hr>
+    <div>
+      <h1>{{ count }}</h1>
+      <button @click="handleIncrease" class="btn btn-success">Tăng</button>
+      <button @click="handleDecrease" class="btn btn-danger">Giảm</button>
+      <button @click="handleReset" class="btn btn-warning">Reset</button>
+    </div>
+    <hr>
+    <ul>
+      <li>{{ myArray[0] }}</li>
+      <li>{{ myArray[1] }}</li>
+      <li>{{ myArray[2] }}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+/* scoped khai phạm css chỉ thỏa mãn trong file vue hiện tại */
+</style>
