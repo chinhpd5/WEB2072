c2666da056087d99e9953d794ca39321371e85d7
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-07-17 14:40:21 +0700

    reactive

--- web2072/src/App.vue
+++ web2072/src/App.vue
@@ -1,30 +1,56 @@
 <script setup>
-import {ref} from 'vue'
+import {ref,reactive} from 'vue'
 
-const name = "chinhpd5";
-const age= 20;
-const gender = true;
+// const name = "chinhpd5";
+// const age= 20;
+// const gender = true;
 
-const count = ref(0);
+const count = ref(0); // có thể sử dụng với tất cả các kiểu dữ liệu
+let info = reactive({ // chỉ sử dụng với array và object
+  name: "chinhpd5",
+  age: 20,
+  gender: true
+})
+// sử dụng ul li hiển thị mảng myArray
+const myArray = reactive(["Hùng", "Long", "Bình"])
+
+const handleChange = () => {
+  info.name = "xin chào chinhpd5"
+
+  // Không thể gán 1 giá trị mới cho reactive
+  // info = { 
+  //   name: "chinhpd5 new",
+  //   age: 20,
+  //   gender: true
+  // } 
+}
 
 const handleIncrease = () => {
   count.value++;
   // console.log(count.value);
 }
 
+const handleDecrease =  () => {
+  count.value--;
+}
+
+const handleReset = () => {
+  count.value = 0;
+}
 </script>
 
 <template>
   <div>
-    <h1 class="text-danger">Họ và tên: {{ name }}</h1>
-    <h2>Tuổi: {{ age }}</h2>
-    <h3>Giới tính: {{ gender ? "Nam": "Nữ" }}</h3>
-
+    <h1 class="text-danger">Họ và tên: {{ info.name }}</h1>
+    <h2>Tuổi: {{ info.age }}</h2>
+    <h3>Giới tính: {{ info.gender ? "Nam": "Nữ" }}</h3>
+    <button class="btn btn-info" @click="handleChange">Change</button>
+    <hr>
     <div>
       <h1>{{ count }}</h1>
       <button @click="handleIncrease" class="btn btn-success">Tăng</button>
-      <button class="btn btn-danger">Giảm</button>
-      <button class="btn btn-warning">Reset</button>
+      <button @click="handleDecrease" class="btn btn-danger">Giảm</button>
+      <button @click="handleReset" class="btn btn-warning">Reset</button>
     </div>
   </div>
 </template>
