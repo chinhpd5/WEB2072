a19b6cc0ccb032f5efa4d5eb5d22e15dd4ad5179
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-07-17 15:28:50 +0700

    to do list

--- web2072/src/App.vue
+++ web2072/src/App.vue
@@ -9,18 +9,34 @@
     <!-- two-way binding -->
     <input type="text" v-model="text">
     <h1>Content: {{ text }}</h1>
+    <hr>
+    <h1>Todo list:</h1>
+
+    <input type="text" v-model="job">
+    <button v-on:click="handleAddJob" class="btn btn-primary">Add job</button>
+    <ul>
+      <li v-for="(item,index) in jobs" :key="index">
+        {{ item }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup>
-
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 const name = "chinhpd5";
 const imageURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGsY-zxNnrhZxqj4a_6Kvuv_tU1oGJWdAY1g&s"
 
-const text = ref("chinhpd5")
+const text = ref("chinhpd5");
+const jobs = reactive(["Ăn", "Ngủ", "Code"])
+const job = ref("");
 
+const handleAddJob = () => {
+  // console.log(job.value);
+  jobs.push(job.value)
+  job.value = ""
+}
 </script>
 
 <style>
