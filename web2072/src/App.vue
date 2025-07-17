aa0205e327d7d9a65e3e9b0cc977eb1d97823507
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-07-17 15:14:11 +0700

    two-way binding

--- web2072/src/App.vue
+++ web2072/src/App.vue
@@ -4,12 +4,23 @@
     <h1>Họ và tên: {{ name }}</h1>
     <img height="100" v-bind:src="imageURL" v-bind:alt="name">
     <img height="100" :src="imageURL" :alt="name">
+    <hr>
+
+    <!-- two-way binding -->
+    <input type="text" v-model="text">
+    <h1>Content: {{ text }}</h1>
   </div>
 </template>
 
 <script setup>
+
+import { ref } from 'vue';
+
 const name = "chinhpd5";
 const imageURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGsY-zxNnrhZxqj4a_6Kvuv_tU1oGJWdAY1g&s"
+
+const text = ref("chinhpd5")
+
 </script>
 
 <style>
