bff57c3251deb9b0f240eb15489b08656dcc2630
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-07-19 14:43:06 +0700

    v-show

--- web2072/src/App.vue
+++ web2072/src/App.vue
@@ -9,13 +9,23 @@
       <span v-else-if="mark >= 5">Trung bình</span>
       <span v-else>Yếu</span>
     </h2>
+
+    <hr>
+    <!-- show -->
+    <button v-show="!isLogin" @click="isLogin = !isLogin" class="btn btn-primary">Đăng nhập</button>
+    <div v-show="isLogin">
+      <h3>Xin chào, admin</h3>
+      <button @click="isLogin = !isLogin" class="btn btn-outline-danger">Đăng xuất</button>
+    </div>
+
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
 const mark = Math.floor(Math.random()*10);
-
+const isLogin = ref(false);
 </script>
 
 <style>
