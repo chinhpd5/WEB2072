f7000a62980e7ff0588f333fe0cdbf84537c6fb2
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-07-19 14:29:43 +0700

    if esle

--- web2072/src/App.vue
+++ web2072/src/App.vue
@@ -1,48 +1,21 @@
 <template>
   <div>
-    <!-- one-way binding -->
-    <h1>Họ và tên: {{ name }}</h1>
-    <img height="100" v-bind:src="imageURL" v-bind:alt="name">
-    <img height="100" :src="imageURL" :alt="name">
-    <hr>
-
-    <!-- two-way binding -->
-    <input type="text" v-model="text">
-    <h1>Content: {{ text }}</h1>
-    <hr>
-    <h1>Todo list:</h1>
-
-    <input type="text" v-model="job">
-    <button v-on:click="handleAddJob" class="btn btn-primary">Add job</button>
-    <ul>
-      <li v-for="(item,index) in jobs" :key="index">
-        {{ item }}
-        <button @click="handleDeleteJob(index)" class="btn btn-danger">Xóa</button>
-      </li>
-    </ul>
+    <!-- if else -->
+    <h1>Điểm: {{ mark }}</h1>
+    <h2>
+      Học lực:
+      <span v-if="mark >= 9">Giỏi</span>
+      <span v-else-if="mark >= 7">Khá</span>
+      <span v-else-if="mark >= 5">Trung bình</span>
+      <span v-else>Yếu</span>
+    </h2>
   </div>
 </template>
 
 <script setup>
-import { reactive, ref } from 'vue';
 
-const name = "chinhpd5";
-const imageURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGsY-zxNnrhZxqj4a_6Kvuv_tU1oGJWdAY1g&s"
+const mark = Math.floor(Math.random()*10);
 
-const text = ref("chinhpd5");
-const jobs = reactive(["Ăn", "Ngủ", "Code"])
-const job = ref("");
-
-const handleAddJob = () => {
-  // console.log(job.value);
-  jobs.push(job.value)
-  job.value = ""
-}
-
-const handleDeleteJob = (index) => {
-  // console.log(index);
-  jobs.splice(index,1);
-}
 </script>
 
 <style>
---
+++ web2072/src/components/binding.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <!-- one-way binding -->
+    <h1>Họ và tên: {{ name }}</h1>
+    <img height="100" v-bind:src="imageURL" v-bind:alt="name">
+    <img height="100" :src="imageURL" :alt="name">
+    <hr>
+
+    <!-- two-way binding -->
+    <input type="text" v-model="text">
+    <h1>Content: {{ text }}</h1>
+    <hr>
+    <h1>Todo list:</h1>
+
+    <input type="text" v-model="job">
+    <button v-on:click="handleAddJob" class="btn btn-primary">Add job</button>
+    <ul>
+      <li v-for="(item,index) in jobs" :key="index">
+        {{ item }}
+        <button @click="handleDeleteJob(index)" class="btn btn-danger">Xóa</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue';
+
+const name = "chinhpd5";
+const imageURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGsY-zxNnrhZxqj4a_6Kvuv_tU1oGJWdAY1g&s"
+
+const text = ref("chinhpd5");
+const jobs = reactive(["Ăn", "Ngủ", "Code"])
+const job = ref("");
+
+const handleAddJob = () => {
+  // console.log(job.value);
+  jobs.push(job.value)
+  job.value = ""
+}
+
+const handleDeleteJob = (index) => {
+  // console.log(index);
+  jobs.splice(index,1);
+}
+</script>
+
+<style>
+
+</style>
