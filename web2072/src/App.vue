09917030e68d9316572486ec3e605a83404d7e35
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-07-24 15:21:35 +0700

    product list

--- web2072/src/App.vue
+++ web2072/src/App.vue
@@ -1,16 +1,88 @@
 <template>
   <div>
     <!-- <MyComponent></MyComponent> -->
-    <MyComponent :name="myName" :age="myAge" :gender="myGender"/>
-    <MyComponent name="Nguyễn Văn A" :age="21" :gender="false" />
+    <!-- <MyComponent :name="myName" :age="myAge" :gender="myGender"/>
+    <MyComponent name="Nguyễn Văn A" :age="21" :gender="false" /> -->
+    <ProductList :list="products" />
   </div>
 </template>
 
 <script setup>
 import MyComponent from './components/MyComponent.vue';
+import ProductList from './components/ProductList.vue';
+
 const myName = "chinhpd5";
 const myAge = 20;
 const myGender = true;
+const products = [
+  {
+    id: 1,
+    name: 'Laptop Acer Aspire 7',
+    description: 'Laptop gaming giá rẻ cho sinh viên.',
+    price: 15000000,
+    inStock: true,
+    category: 'Laptop',
+    rating: 4.2,
+    imageUrl: 'https://cdn.hoanghamobile.com/i/previewV2/Uploads/2022/10/03/image-removebg-preview-60.png',
+    tags: ['gaming', 'student', 'budget'],
+    quantity: 10,
+    featured: true
+  },
+  {
+    id: 2,
+    name: 'MacBook Air M1',
+    description: 'Laptop nhẹ, hiệu năng cao cho lập trình viên.',
+    price: 22000000,
+    inStock: false,
+    category: 'Laptop',
+    rating: 3.8,
+    imageUrl: 'https://cdn.hoanghamobile.com/i/previewV2/Uploads/2024/11/16/mac-m1-ghi-1.png',
+    tags: ['apple', 'm1', 'developer'],
+    quantity: 0,
+    featured: false
+  },
+  {
+    id: 3,
+    name: 'Dell XPS 13',
+    description: 'Thiết kế cao cấp, hiệu năng ổn định.',
+    price: 28000000,
+    inStock: true,
+    category: 'Laptop',
+    rating: 2.6,
+    imageUrl: 'https://cdn.hoanghamobile.com/i/previewV2/Uploads/2024/10/17/71034921-1.png',
+    tags: ['premium', 'portable'],
+    quantity: 5,
+    featured: true
+  },
+  {
+    id: 4,
+    name: 'HP Pavilion 15',
+    description: 'Lựa chọn tầm trung cho công việc văn phòng.',
+    price: 17000000,
+    inStock: true,
+    category: 'Laptop',
+    rating: 4.0,
+    imageUrl: 'https://cdn.hoanghamobile.com/i/previewV2/Uploads/2023/12/22/hp-15s-silver-1.png',
+    tags: ['office', 'midrange'],
+    quantity: 8,
+    featured: false
+  },
+  {
+    id: 5,
+    name: 'Lenovo IdeaPad 3',
+    description: 'Máy tính xách tay cơ bản cho học sinh.',
+    price: 12000000,
+    inStock: false,
+    category: 'Laptop',
+    rating: 3.9,
+    imageUrl: 'https://cdn.hoanghamobile.com/i/previewV2/Uploads/2024/07/11/lenovo-loq-15iax9-83fq0005vn-1.png',
+    tags: ['basic', 'student'],
+    quantity: 0,
+    featured: false
+  },
+];
+
+
 </script>
 
 <style scoped>
---
+++ web2072/src/components/ProductList.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <h1>Danh sách sản phẩm</h1>
+    <table class="table table-bordered table-striped table-hover">
+      <thead>
+        <tr>
+          <th scope="col">STT</th>
+          <th scope="col">Tên sản phẩm</th>
+          <th scope="col">Hình ảnh</th>
+          <th scope="col">Số lượng</th>
+          <th scope="col">Giá bán</th>
+          <th scope="col">Danh mục</th>
+          <th scope="col">Đánh giá</th>
+          <th scope="col">Tag</th>
+          <th scope="col">Yêu thích</th>
+          <th scope="col">Mô tả</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item,index) in list" :key="item.id">
+          <th scope="row">{{ index+1 }}</th>
+          <td>{{ item.name }}</td>
+          <td>
+            <img height="50" :src="item.imageUrl" alt="">
+          </td>
+          <td>
+            <span v-if="item.inStock">{{ item.quantity }}</span>
+            <span v-else>Hết hàng</span>
+          </td>
+          <td>{{ item.price }}</td>
+          <td>{{ item.category }}</td>
+          <td>
+            <span v-if="item.rating ==5 ">⭐⭐⭐⭐⭐</span>
+            <span v-else-if="item.rating >= 4">⭐⭐⭐⭐</span>         
+            <span v-else-if="item.rating >= 3">⭐⭐⭐</span>         
+            <span v-else-if="item.rating >= 2">⭐⭐</span>         
+            <span v-else-if="item.rating >= 1">⭐</span> 
+            <span v-else></span>       
+          </td>
+          <td>
+            <span v-for="(tag,i) in item.tags" :key="i">
+              {{ tag }}, 
+            </span>
+          </td>
+          <td>
+            <span v-show="item.featured">Sản phẩm nổi bật</span>
+          </td>
+          <td>{{ item.description }}</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  list: Array,
+});
+
+// console.log(props);
+</script>
+
+<style></style>
