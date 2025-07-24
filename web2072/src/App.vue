dc4440838e6a6e4130c9789672de2b119cfa1795
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-07-24 14:50:57 +0700

    Props

--- web2072/src/App.vue
+++ web2072/src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <MyComponent></MyComponent>
-    <MyComponent />
+    <!-- <MyComponent></MyComponent> -->
+    <MyComponent :name="myName" :age="myAge" :gender="myGender"/>
+    <MyComponent name="Nguyễn Văn A" :age="21" :gender="false" />
   </div>
 </template>
 
 <script setup>
-import MyComponent from './components/MyComponent.vue'
+import MyComponent from './components/MyComponent.vue';
+const myName = "chinhpd5";
+const myAge = 20;
+const myGender = true;
 </script>
 
 <style scoped>
--- web2072/src/components/MyComponent.vue
+++ web2072/src/components/MyComponent.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <h1>Họ và tên: Phí Đức Chính</h1>
-    <h2>Tuổi: 20</h2>
-    <h3>Giới tính: nam</h3>
+    <h1>Họ và tên: {{ name }}</h1>
+    <h2>Tuổi: {{ age }}</h2>
+    <h3>Giới tính: {{ gender ? "nam": "nữ" }}</h3>
   </div>
 </template>
 
 <script setup>
+const props = defineProps({
+  name: String,
+  age: Number,
+  gender: Boolean
+})
 
+console.log(props);
 </script>
 
 <style scoped>
