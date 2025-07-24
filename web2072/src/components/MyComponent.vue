13139194a891e8e39d710b976407b05d43daea05
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-07-24 14:34:54 +0700

    component

---
+++ web2072/src/components/MyComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Họ và tên: Phí Đức Chính</h1>
+    <h2>Tuổi: 20</h2>
+    <h3>Giới tính: nam</h3>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ web2072/src/components/directive.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <!-- if else -->
+    <h1>Điểm: {{ mark }}</h1>
+    <h2>
+      Học lực:
+      <span v-if="mark >= 9">Giỏi</span>
+      <span v-else-if="mark >= 7">Khá</span>
+      <span v-else-if="mark >= 5">Trung bình</span>
+      <span v-else>Yếu</span>
+    </h2>
+
+    <hr>
+    <!-- show -->
+    <button v-show="!isLogin" @click="isLogin = !isLogin" class="btn btn-primary">
+      Đăng nhập
+    </button>
+
+    <div v-show="isLogin">
+      <h3>Xin chào, admin</h3>
+      <button @click="isLogin = !isLogin" class="btn btn-outline-danger">
+        Đăng xuất
+      </button>
+    </div>
+
+    <hr>
+    <!-- for -->
+    <ul>
+      <li v-for="(item, index) in myArray" :key="index">
+        {{ item.name }} - {{ item.age }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const mark = Math.floor(Math.random()*10);
+const isLogin = ref(false);
+const myArray = [
+  {name: "Nguyễn Văn A", age: 20},
+  {name: "Nguyễn Văn B", age: 21},
+  {name: "Nguyễn Văn C", age: 22}
+];
+</script>
+
+<style>
+</style>
