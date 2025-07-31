2837ce8fdf21b2e3a25d30171bedc0c68f463969
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-07-31 14:29:42 +0700

    tạo view list

---
+++ web2072/src/layouts/AdminLayout.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="container">
+    <div class="sidebar d-flex flex-column p-3">
+      <h4 class="text-white mb-4">Admin Panel</h4>
+      <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link" aria-current="page">Dashboard</router-link>
+        </li>
+        <li>
+          <router-link to="/" class="nav-link">Quản lý người dùng</router-link>
+        </li>
+        <li>
+          <router-link to="/admin/product" class="nav-link">Sản phẩm</router-link>
+        </li>
+        <li>
+          <router-link to="#" class="nav-link">Đơn hàng</router-link>
+        </li>
+        <li>
+          <router-link to="#" class="nav-link">Cài đặt</router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="content">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#container {
+  display: flex;
+}
+
+.sidebar {
+  position: sticky;
+  top: 0;
+  width: 250px;
+  background-color: #343a40;
+  color: #fff;
+  flex-shrink: 0;
+  height: 100vh;
+}
+
+.sidebar .nav-link {
+  color: #fff;
+}
+
+.content {
+  flex-grow: 1;
+  padding: 20px;
+}
+</style>
--- web2072/src/router/index.js
+++ web2072/src/router/index.js
@@ -3,14 +3,21 @@ import HomePage from '../pages/HomePage.vue';
 import ProductPage from '../pages/ProductPage.vue';
 import ClientLayout from '../layouts/ClientLayout.vue';
 import ProductDetailPage from '../pages/ProductDetailPage.vue';
+import AdminLayout from '../layouts/AdminLayout.vue';
+import ListProduct from '../pages/product/List.vue';
 
 const router = createRouter({
   routes: [
     {
       path: '/', component: ClientLayout, children: [
         {path: '/', name: 'home-page', component: HomePage},
-        {path: '/product', name: 'product-page', component: ProductPage},
-        {path: '/product/:id', name: 'product-detail-page', component: ProductDetailPage},
+        {path: 'product', name: 'product-page', component: ProductPage},
+        {path: 'product/:id', name: 'product-detail-page', component: ProductDetailPage},
+      ]
+    },
+    {
+      path: '/admin', component: AdminLayout, children: [
+        {path: 'product', component: ListProduct, name: 'product-list'}
       ]
     }
   ],
